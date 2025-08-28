ce0db1cf4650573d3fafa71b6fb13afb1d7ae1eb
MalemaDev <lpeepp030@gmail.com>
2025-08-27 19:47:19 -0500

    Quiz determinar numeros primos

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,17 @@
 fun main() {
-    val numbers = listOf(1, 2, 4, 7, 13, 9, 23)
+    val numeros = listOf(1, 2, 4, 7, 13, 9, 23)
+    val resultado = numeros.map { esPrimo(it) }
 
-    // map applies the isPrime function to each element of the list
-    val result = numbers.map { isPrime(it) }
-
-    println("Numbers: $numbers")
-    println("Are they prime?: $result")
+    println("IN: $numeros")
+    println("OUT: $resultado")
 }
 
-/*
-TODO: Function that checks if a number is prime
- */
-fun isPrime(n: Int): Boolean {
-    return true;
+
+
+fun esPrimo(n: Int): Boolean {
+    if (n < 2) return false
+    for (i in 2..Math.sqrt(n.toDouble()).toInt()) {
+        if (n % i == 0) return false
+    }
+    return true
 }
